072d6b470f8f9b936b72b267da53f95208603a11
M.Abubakar <159885125+abubakar-maker@users.noreply.github.com>
2024-05-22 11:43:14 +0500

    Add files via upload

---
+++ mispoci coding project/main.js
@@ -0,0 +1,11 @@
+let subBtn = () => {
+  window.location.href = `subscribe.html`;
+};
+
+let joinBtn = () => {
+  window.location.href = `join.html`;
+};
+
+let refresh = () => {
+  window.location = `index.html`;
+};
